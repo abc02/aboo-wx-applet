eecba6344d59638871a4cf7c3c7c2229317c6bee
abc02 <abcgg02@gmail.com>
2017-12-26 14:27:44 +0800

    新增 UseApp & FAQ 完成 FAQ

--- src/mixins/fixing.js
+++ src/mixins/fixing.js
@@ -20,53 +20,68 @@ export default class Fixing extends Wepy.mixin {
   }
   // 获取用户绑定信息
   async GetBindinginfo() {
-    let res = await FixingService.GetBindinginfo(this.JoinFixingRequest())
-    return res
+    return await FixingService.GetBindinginfo(this.JoinFixingRequest())
   }
   // 获取设备最近一次历史定位数据
   async GetLastPosition(fixing) {
     let FixingLocationInfo = await FixingService.GetLastPosition(this.JoinFixingRequest(fixing))
-    if (FixingLocationInfo.ret !== 1001) return Tips.toast('定位获取失败')
-    // 转换经纬度
-    let TencentLocationInfo = await this.GetBaiduToTencentLocationInfo(FixingLocationInfo.data.Position)
-    let res = this.SetMarkers(TencentLocationInfo.result, FixingLocationInfo.data)
-    return res
+    return await this.GetLastPositonResponse(FixingLocationInfo)
   }
   // 获取设备定位数据
   async GetLastPositionSmall(fixing) {
-    console.log(fixing)
     let FixingLocationInfo = await FixingService.GetLastPositionSmall(this.JoinFixingRequest(fixing))
-    if (FixingLocationInfo.data.Ret !== 1001) return Tips.toast('定位获取失败')
-
-    // 1003 初始化 1002 定位失败 1001 定位成功    13868623153  884127
-
-    // 转换经纬度
-    let TencentLocationInfo = await this.GetBaiduToTencentLocationInfo(FixingLocationInfo.data.Position)
-    let res = this.SetMarkers(TencentLocationInfo.result, FixingLocationInfo.data)
-    return res
+    return await this.GetLastPositonResponse(FixingLocationInfo)
   }
-  SetMarkers(markers, fixing, mssage) {
+  async GetLastPositonResponse(res) {
+    let exp
+    res.ret ? exp = res.ret : exp = res.data.Ret
+    switch (exp) {
+      // 1003 初始化 1002 定位失败 1001 定位成功    13868623153  884127
+      case 1001: // 定位成功
+        // 转换经纬度
+        let TencentLocationInfo = await this.GetBaiduToTencentLocationInfo(res.data.Position)
+        return this.SetMarkers({message: '定位成功', iconPath: './assets/icon_gif_success.png'}, TencentLocationInfo.result, res.data)
+      case 1002:// 定位失败
+        return this.SetMarkers({message: '定位失败', iconPath: './assets/icon_gif_fail.png'})
+      case 1003:// 正在初始化
+        return this.SetMarkers({message: '正在初始化', iconPath: './assets/icon_gif_init.png'})
+    }
+  }
+  SetMarkers(message, TencentLocationInfo = null, fixingInfo = null) {
     let res = {
       id: 0,
-      iconPath: './assets/location_animte.gif',
+      iconPath: message.iconPath,
       width: 50,
-      height: 50,
-      latitude: markers.location.lat,
-      longitude: markers.location.lng,
-      ...markers,
-      ...fixing,
-      label: {
-        content: '测试测试',
-        fontSize: 16,
-        x: -25,
-        y: -80,
-        borderWidth: 1,
-        borderColor: '#f3f3f3',
-        borderRadius: 6,
-        bgColor: '#ffffff',
-        padding: 3
+      height: 50
+    }
+    let label = {
+      content: message.message,
+      fontSize: 16,
+      x: -25,
+      y: -80,
+      borderWidth: 1,
+      borderColor: '#f3f3f3',
+      borderRadius: 6,
+      bgColor: '#ffffff',
+      padding: 3
+    }
+    if (TencentLocationInfo && fixingInfo) {
+      res = {
+        ...res,
+        latitude: TencentLocationInfo.location.lat,
+        longitude: TencentLocationInfo.location.lng,
+        ...TencentLocationInfo,
+        ...fixingInfo,
+        label
+      }
+    } else {
+      res = {
+        ...this.markers[0],
+        ...res,
+        label
       }
     }
+    console.log(res)
     return res
   }
   // 调用接口 latitude, longitude 百度 -> 腾讯
@@ -89,13 +104,13 @@ export default class Fixing extends Wepy.mixin {
       })
     })
   }
+  // 设备解绑
   async Unbind(fixing) {
-    let res = await FixingService.Unbind(this.JoinFixingRequest(fixing))
-    return res
+    return await FixingService.Unbind(this.JoinFixingRequest(fixing))
   }
+  // 设备默认
   async UpdateFixingIsDefault(fixing) {
-    let res = await FixingService.UpdateFixingIsDefault(this.JoinFixingRequest(fixing))
-    return res
+    return await FixingService.UpdateFixingIsDefault(this.JoinFixingRequest(fixing))
   }
   onLoad() {
      // 实例化API核心类
